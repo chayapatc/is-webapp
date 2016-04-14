a676240fd3e864f68caf2a558356f0f0ee444e11
Chayapat Chadilalai <chayapatc@github.com>
2016-04-14 13:11:34 +0700

    Update routes

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
-import App from './modules/App'
-import About from './modules/About'
-import Repos from './modules/Repos'
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import App from './modules/App';
+import Home from './modules/Home';
+import Product from './modules/Product';
+import Cart from './modules/Cart';
+import Checkout from './modules/Checkout';
+import CheckoutComplete from './modules/CheckoutComplete';
 
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}/>
-    <Route path="/repos" component={Repos}/>
-    <Route path="/about" component={About}/>
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    	<IndexRoute component={Home}/>
+  		<Route path="/product/:id" component={Product}/>
+    	<Route path="/cart" component={Cart}/>
+    	<Route path="/checkout" component={Checkout}/>
+    	<Route path="/checkout/complete" component={CheckoutComplete}/>
+    </Route>
   </Router>
-), document.getElementById('app'))
+), document.getElementById('app'));
